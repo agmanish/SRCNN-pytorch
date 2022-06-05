d6d8b4279bd9cf590058d8ab8514fe66ff2adcf4
Manish A.G <62322881+agmanish@users.noreply.github.com>
2022-06-05 20:41:32 +0530

    Update train.py

--- train.py
+++ train.py
@@ -60,6 +60,7 @@ if __name__ == '__main__':
     best_weights = copy.deepcopy(model.state_dict())
     best_epoch = 0
     best_psnr = 0.0
+    best_ssim = 0.0
     loss_dict={}
     psnr_dict={}
     ssim_dict={}
@@ -132,7 +133,7 @@ if __name__ == '__main__':
         "ssim_vs_epoch":ssim_dict,
         "best_epoch":best_epoch,
         "best_psnr":best_psnr,
-        #"best_ssim":best_ssim
+        "best_ssim":best_ssim
     }
     json_path=args.opm_dir+"/train_metrics.json"
     with open(json_path, "w") as outfile:
