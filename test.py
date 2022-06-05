099b25c65b718f21c844104d1d14e0ad989fe305
Manish A.G <62322881+agmanish@users.noreply.github.com>
2022-06-05 20:19:41 +0530

    Update test.py

--- test.py
+++ test.py
@@ -38,53 +38,54 @@ if __name__ == '__main__':
     ssim_dict={}
     
     for file in filenames:
-      image = pil_image.open(os.path.join(ip_path,file)).convert('RGB')
-    
-      orig= np.array(image).astype(np.float32)
-      orig=convert_rgb_to_ycbcr(orig)  
-      orig = orig[..., 0]
-      orig /= 255.
-      orig = torch.from_numpy(orig).to(device)
-      oig = orig.unsqueeze(0).unsqueeze(0)
+          image = pil_image.open(os.path.join(ip_path,file)).convert('RGB')
 
-      
-      image_width = (image.width // args.scale) * args.scale
-      image_height = (image.height // args.scale) * args.scale
-      image = image.resize((image_width, image_height), resample=pil_image.BICUBIC)
-      image = image.resize((image.width // args.scale, image.height // args.scale), resample=pil_image.BICUBIC)
-      image = image.resize((image.width * args.scale, image.height * args.scale), resample=pil_image.BICUBIC)
-      op_name=os.path.join(args.op_dir,file)
-      image.save(op_name.replace('.', '_bicubic_x{}.'.format(args.scale)))
+          orig= np.array(image).astype(np.float32)
+          orig=convert_rgb_to_ycbcr(orig)  
+          orig = orig[..., 0]
+          orig /= 255.
+          orig = torch.from_numpy(orig).to(device)
+          oig = orig.unsqueeze(0).unsqueeze(0)
 
-      image = np.array(image).astype(np.float32)
-      ycbcr = convert_rgb_to_ycbcr(image)
 
-      y = ycbcr[..., 0]
-      y /= 255.
-      y = torch.from_numpy(y).to(device)
-      y = y.unsqueeze(0).unsqueeze(0)
-      
+          image_width = (image.width // args.scale) * args.scale
+          image_height = (image.height // args.scale) * args.scale
+          image = image.resize((image_width, image_height), resample=pil_image.BICUBIC)
+          image = image.resize((image.width // args.scale, image.height // args.scale), resample=pil_image.BICUBIC)
+          image = image.resize((image.width * args.scale, image.height * args.scale), resample=pil_image.BICUBIC)
+          op_name=os.path.join(args.op_dir,file)
+          image.save(op_name.replace('.', '_bicubic_x{}.'.format(args.scale)))
 
-      with torch.no_grad():
-          preds = model(y).clamp(0.0, 1.0)
-      print(orig.size(),preds.size())
-      orig=orig.view(1,1,orig.size()[0],orig.size()[1])
-      print(orig.size(),preds.size())
-      psnr = calc_psnr(orig, preds)
-      ssim = calc_ssim(orig, preds)
-      psnr_dict[file] = psnr
-      psnr_dict[file] = ssim
-      print('PSNR: {:.2f}'.format(psnr))
-      print('SSIM: {:.2f}'.format(ssim))
+          image = np.array(image).astype(np.float32)
+          ycbcr = convert_rgb_to_ycbcr(image)
 
-      preds = preds.mul(255.0).cpu().numpy().squeeze(0).squeeze(0)
+          y = ycbcr[..., 0]
+          y /= 255.
+          y = torch.from_numpy(y).to(device)
+          y = y.unsqueeze(0).unsqueeze(0)
 
-      output = np.array([preds, ycbcr[..., 1], ycbcr[..., 2]]).transpose([1, 2, 0])
-      output = np.clip(convert_ycbcr_to_rgb(output), 0.0, 255.0).astype(np.uint8)
-      output = pil_image.fromarray(output)
-      output.save(op_name.replace('.', '_srcnn_x{}.'.format(args.scale)))
-    
-    train_metrics={
+
+          with torch.no_grad():
+              preds = model(y).clamp(0.0, 1.0)
+          print(orig.size(),preds.size())
+          orig=orig.view(1,1,orig.size()[0],orig.size()[1])
+          print(orig.size(),preds.size())
+          psnr = calc_psnr(orig, preds)
+          ssim = calc_ssim(orig, preds)
+          
+          print('PSNR: {:.2f}'.format(psnr))
+          print('SSIM: {:.2f}'.format(ssim))
+
+          preds = preds.mul(255.0).cpu().numpy().squeeze(0).squeeze(0)
+
+          output = np.array([preds, ycbcr[..., 1], ycbcr[..., 2]]).transpose([1, 2, 0])
+          output = np.clip(convert_ycbcr_to_rgb(output), 0.0, 255.0).astype(np.uint8)
+          output = pil_image.fromarray(output)
+          output.save(op_name.replace('.', '_srcnn_x{}.'.format(args.scale)))
+          psnr_dict[file] = psnr
+          ssim_dict[file] = ssim
+          print(psnr_dict[file], ssim_dict[file])
+    test_metrics={
         "psnr_vs_epoch":psnr_dict,
         "ssim_vs_epoch":ssim_dict,
     }
