299f78797620d1956f2dba407bd78945ba81b6f2
Manish A.G <62322881+agmanish@users.noreply.github.com>
2022-06-05 20:06:32 +0530

    Update test.py

--- test.py
+++ test.py
@@ -67,7 +67,7 @@ if __name__ == '__main__':
       print(y.shape)
       y /= 255.
       y = torch.from_numpy(y).to(device)
-      print(orig.size(),,y.size())
+      print(orig.size(),y.size())
       y = y.unsqueeze(0).unsqueeze(0)\
       print(orig.size(),,y.size())
 
