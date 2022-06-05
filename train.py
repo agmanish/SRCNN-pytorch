4829494fa51a67c38ccaac35808c251eb8d06357
Manish A.G <62322881+agmanish@users.noreply.github.com>
2022-06-05 17:37:40 +0530

    Update train.py

--- train.py
+++ train.py
@@ -133,7 +133,7 @@ if __name__ == '__main__':
         "best_epoch":best_epoch,
         "best_psnr":best_psnr,
         "best_ssim":best_ssim
-}
-json_path=args.opm-dir+"/train_metrics.json"
-with open(json_path, "w") as outfile:
-    json.dump(train_metrics, outfile)
+    }
+    json_path=args.opm-dir+"/train_metrics.json"
+    with open(json_path, "w") as outfile:
+        json.dump(train_metrics, outfile)
